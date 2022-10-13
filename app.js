c950f758da97c3c00814d70b46a612e44a7f1ece
Aman Akhtar <77399188+amanakhtar78@users.noreply.github.com>
2022-10-13 17:40:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const colors = ["green", "black","red", "rgba(133,122,200)", "#f15025"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
